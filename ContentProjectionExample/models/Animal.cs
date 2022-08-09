bc968616d754f3fa94c8b371d6e07ca8bbcd493c
Bian Yu Wang <bianyu.wang@gmail.com>
2022-08-09 15:10:48 -0400

    Understand Generic Projection

---
+++ ContentProjectionExample/models/Animal.cs
@@ -0,0 +1,21 @@
+﻿namespace ContentProjectionExample.models
+{
+    public interface IAnimal
+    {
+        string Name { get; set; }
+    }
+
+    public class Dog : IAnimal
+    {
+        public string? Name { get; set; }
+    }
+    public class Cat : IAnimal
+    {
+        public string? Name { get; set; }
+    }
+    public class Bird : IAnimal
+    {
+        public string? Name { get; set; }
+    }
+
+}
